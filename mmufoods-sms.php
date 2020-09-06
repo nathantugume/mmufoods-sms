10703eb762ce4c8937de4f28c68e20094b5ec869
nathan tugume <41728039+nathantugume@users.noreply.github.com>
2020-09-06 14:58:51 +0300

    Update mmufoods-sms.php

--- mmufoods-sms.php
+++ mmufoods-sms.php
@@ -1,1 +1,9 @@
-<
+<?php
+/**
+ * Plugin Name: mmufoods-sms
+ * Plugin URI: https://github.com/nathantugume/mmufoods-sms/
+ * Author: Nathan
+ * Author URI: https://github.com/nathantugume/mmufoods-sms/
+ * Description: Send SMS notifications from woocommerce Order notifications upon status change.
+ * Version: 0.0.1
+ */
